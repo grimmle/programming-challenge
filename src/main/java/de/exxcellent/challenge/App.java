0c8d3387a08104f126874703bf0f942d2fc750c1
Lennard Grimm <hi@grimmle.com>
2022-10-03 17:34:51 +0200

    read data and display analysis result

--- src/main/java/de/exxcellent/challenge/App.java
+++ src/main/java/de/exxcellent/challenge/App.java
@@ -1,5 +1,12 @@
 package de.exxcellent.challenge;
 
+import java.io.*;
+import java.util.*;
+
+import static de.exxcellent.challenge.FootballData.sortTeamsBySmallestGoalSpread;
+import static de.exxcellent.challenge.WeatherData.sortDaysBySmallestTempSpread;
+
+
 /**
  * The entry class for your solution. This class is only aimed as starting point and not intended as baseline for your software
  * design. Read: create your own classes and packages as appropriate.
@@ -13,13 +20,24 @@ public final class App {
      * @param args The CLI arguments passed
      */
     public static void main(String... args) {
+        // create custom FileReader to read file contents
+        FileReader fr = new FileReader();
 
-        // Your preparation code …
+        // WEATHER DATASET
+        List<List<String>> weatherData = fr.getFileData("de/exxcellent/challenge/weather.csv");
+        // sorting days by smallest temperature spread
+        weatherData.sort(sortDaysBySmallestTempSpread);
+        // get id of day at index 0
+        String dayWithSmallestTempSpread = weatherData.get(0).get(0).toString();
+        System.out.printf("Day with smallest temperature spread: %s%n", dayWithSmallestTempSpread);
 
-        String dayWithSmallestTempSpread = "Someday";     // Your day analysis function call …
-        System.out.printf("Day with smallest temperature spread : %s%n", dayWithSmallestTempSpread);
 
-        String teamWithSmallestGoalSpread = "A good team"; // Your goal analysis function call …
-        System.out.printf("Team with smallest goal spread       : %s%n", teamWithSmallestGoalSpread);
+        // FOOTBALL DATASET
+        List<List<String>> footballData = fr.getFileData("de/exxcellent/challenge/football.csv");
+        // sorting teams by absolute goal difference
+        footballData.sort(sortTeamsBySmallestGoalSpread);
+        // get name of team at index 0
+        String teamWithSmallestGoalSpread = footballData.get(0).get(0).toString();
+        System.out.printf("Team with smallest goal spread: %s%n", teamWithSmallestGoalSpread);
     }
 }
